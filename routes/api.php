02429c67fd88f54b9e612e52c169e7020b69a124
Velyzar <artembronskiy@gmail.com>
2026-06-04 17:46:00 +0000

    feat: Лаб 8. Реалізовано BlogPostRepository та вивід статей блогу в адмінці

--- routes/api.php
+++ routes/api.php
@@ -4,26 +4,31 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Blog\PostController;
 use App\Http\Controllers\Api\Blog\Admin\CategoryController;
+use App\Http\Controllers\Api\Blog\Admin\PostController as AdminPostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-
+// Публічна частина блогу (гість)
 Route::group(['prefix' => 'blog'], function () {
     Route::apiResource('posts', PostController::class)->names('blog.posts');
-});
 
-// Адмінка
-Route::group(['prefix' => 'admin/blog'], function () {
-    $methods = ['index', 'store', 'update'];
-    Route::apiResource('categories', \App\Http\Controllers\Api\Blog\Admin\CategoryController::class)
-        ->only($methods)
-        ->names('blog.admin.categories');
-});
-
-Route::group(['prefix' => 'blog'], function () {
     Route::resource('categories', CategoryController::class)
         ->only(['index', 'store', 'update', 'show']);
+});
+
+// Адмінка блогу (те, що нам потрібно для Лаб 8)
+Route::group(['prefix' => 'admin/blog'], function () {
+    $methods = ['index', 'store', 'update'];
+
+    // Категорії в адмінці
+    Route::apiResource('categories', CategoryController::class)
+        ->only($methods)
+        ->names('blog.admin.categories');
+
+    // ЛАБОРАТОРНА 8: Реєстрація адмінських постів
+    Route::apiResource('posts', AdminPostController::class)
+        ->except(['show'])
+        ->names('blog.admin.posts');
 });
