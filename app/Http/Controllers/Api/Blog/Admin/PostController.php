eaeaa257286c5517c3ea2486b56fb10d4ef8e7be
Velyzar <artembronskiy@gmail.com>
2026-06-05 16:10:00 +0000

    feat: Лаб 9. Налаштовано зв'язки моделей та оновлення статей блогу

--- app/Http/Controllers/Api/Blog/Admin/PostController.php
+++ app/Http/Controllers/Api/Blog/Admin/PostController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Api\Blog\Admin;
 
 use App\Repositories\BlogPostRepository;
-use Illuminate\Http\Request;
+use App\Repositories\BlogCategoryRepository;
+use App\Http\Requests\BlogPostUpdateRequest;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 
 class PostController extends BaseController
 {
-    public function __construct(private BlogPostRepository $blogPostRepository)
-    {
+    // Впроваджуємо обидва репозиторії через конструктор
+    public function __construct(
+        private BlogPostRepository $blogPostRepository,
+        private BlogCategoryRepository $blogCategoryRepository
+    ) {
         // parent::__construct();
     }
 
@@ -25,7 +31,7 @@ class PostController extends BaseController
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(\Illuminate\Http\Request $request)
     {
         //
     }
@@ -33,9 +39,34 @@ class PostController extends BaseController
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(BlogPostUpdateRequest $request, string $id)
     {
-        //
+        $item = $this->blogPostRepository->getEdit($id);
+
+        if (empty($item)) { //якщо ід не знайдено
+            return ['message' => "Запис id=[{$id}] не знайдено"];
+        }
+
+        $data = $request->all(); //отримаємо масив даних, які надійшли з форми
+
+        if (empty($data['slug'])) { //якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        }
+
+        if (empty($item->published_at) && !empty($data['is_published'])) { //якщо поле published_at порожнє і нам прийшло 1 в ключі is_published
+            $data['published_at'] = Carbon::now(); //генеруємо поточну дату
+        }
+
+        $result = $item->update($data); //оновлюємо дані об'єкта і зберігаємо в БД
+
+        if ($result) {
+            return [
+                'success' => true,
+                'message' => 'Успішно збережено'
+            ];
+        } else {
+            return ['message' => 'Помилка збереження'];
+        }
     }
 
     /**
